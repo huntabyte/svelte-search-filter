6c7f450a02830e74ef2a6daf1b0556f250359fbe
Hunter Johnston <johnstonhuntera@gmail.com>
2022-12-27 16:44:48 -0500

    final

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
+	import { createSearchStore, searchHandler } from '$lib/stores/search';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	const searchProducts = data.products.map((product: any) => ({
+		...product,
+		searchTerms: `${product.title} ${product.description} ${product.category} ${product.brand}`
+	}));
+
+	const searchStore = createSearchStore(searchProducts);
+
+	searchStore.subscribe((model) => searchHandler(model));
 </script>
 
 <div class="container">
 	<h1>Search/Filter</h1>
-	<input type="search" placeholder="Search..." />
+	<input type="search" placeholder="Search..." bind:value={$searchStore.search} />
 </div>
-<pre>{JSON.stringify(data.products, null, 2)}</pre>
+<div class="product-grid">
+	{#each $searchStore.filtered as product}
+		<div class="product">
+			<h2>{product.title}</h2>
+			<p>{product.description}</p>
+			<p class="badge">{product.category}</p>
+			<p>{product.brand}</p>
+		</div>
+	{/each}
+</div>
 
 <style>
 	.container {
@@ -17,5 +36,37 @@
 		flex-direction: column;
 		align-items: center;
 		padding-top: 1.5rem;
+	}
+
+	.product {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		justify-content: flex-start;
+		margin: 1rem;
+		padding: 1rem;
+		border: 1px solid #ccc;
+		border-radius: 0.5rem;
+		gap: 0.5rem;
+	}
+
+	.product-grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+		grid-gap: 1rem;
+	}
+
+	.badge {
+		display: inline-block;
+		padding: 0.25em 0.4em;
+		font-size: 75%;
+		font-weight: 700;
+		line-height: 1;
+		text-align: center;
+		white-space: nowrap;
+		vertical-align: baseline;
+		border-radius: 0.25rem;
+		background-color: var(--color-primary-500);
+		color: var(--color-gray-1000);
 	}
 </style>
