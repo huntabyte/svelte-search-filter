74c34844ea364823766afacba8c053fa4665b556
Hunter Johnston <johnstonhuntera@gmail.com>
2022-12-27 21:06:46 -0500

    final

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,43 @@
 <script lang="ts">
+	import { createSearchStore, searchHandler } from '$lib/stores/search';
+	import { onDestroy } from 'svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	type Product = {
+		title: string;
+		description: string;
+		brand: string;
+		category: string;
+		searchTerms: string;
+	};
+
+	const searchProducts: Product[] = data.products.map((product: Product) => ({
+		...product,
+		searchTerms: `${product.title} ${product.description} ${product.brand} ${product.category}`
+	}));
+
+	const searchStore = createSearchStore(searchProducts);
+
+	const unsubscribe = searchStore.subscribe((model) => searchHandler(model));
+
+	onDestroy(() => {
+		unsubscribe();
+	});
 </script>
 
 <div class="container">
 	<h1>Search/Filter</h1>
-	<input type="search" placeholder="Search..." />
+	<input type="search" placeholder="Search..." bind:value={$searchStore.search} />
 </div>
-<pre>{JSON.stringify(data.products, null, 2)}</pre>
-
-<style>
-	.container {
-		margin-inline: auto;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		padding-top: 1.5rem;
-	}
-</style>
+<div class="product-grid">
+	{#each $searchStore.filtered as product}
+		<div class="product">
+			<h2>{product.title}</h2>
+			<p>{product.description}</p>
+			<p class="badge">{product.category}</p>
+			<p>{product.brand}</p>
+		</div>
+	{/each}
+</div>
